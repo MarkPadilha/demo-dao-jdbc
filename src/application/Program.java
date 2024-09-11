00ab84bab2d8efc75d751dc54ea761039a6821a8
Mark Padilha <markpadilha10@gmail.com>
2024-09-11 15:51:58 -0300

    Class seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,9 @@ public class Program {
 		
 		Department obj = new Department(1, "livros");
 		
+		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 2000.0, obj );
+		
+		System.out.println(seller);
 		System.out.println(obj);
 	}
 
