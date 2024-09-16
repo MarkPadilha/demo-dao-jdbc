8b8881716e6bb9dd3c76301279153ae2961fe133
Mark Padilha <markpadilha10@gmail.com>
2024-09-16 16:39:43 -0300

    Update

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,13 +34,15 @@ public class Program {
 		}
 		
 		System.out.println("\n=== TEST 4: seller insert === ");
-			Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
-			sellerDao.insert(newSeller);
-			System.out.println("Inserted! new seller id = " + newSeller.getId());
-		
-		
-	
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, dep);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! new seller id = " + newSeller.getId());
 
+		System.out.println("\n=== TEST 5: seller Update === ");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update complete!");
 	}
 
 }
